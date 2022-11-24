e2e4c10198b28e378701c5e5be199ad1ea34d6e6
RobbSpark <ravilmust@mail.ru>
2022-11-24 08:37:42 +0100

    cosmetic corrections

--- test_product_page.py
+++ test_product_page.py
@@ -27,6 +27,7 @@ class TestUserAddToBasketFromProductPage():
 
     LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         self.page = ProductPage(browser, self.LINK)
         self.page.open()
@@ -38,7 +39,7 @@ class TestUserAddToBasketFromProductPage():
         self.page.should_not_be_success_message()
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 @pytest.mark.parametrize('link2', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -86,14 +87,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-@pytest.mark.smoke
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
